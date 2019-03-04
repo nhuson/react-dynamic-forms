76e1833d7c5c13b395231ac9b0c73ec12b22742c
Nhu Son <sonnv@goquo.com>
2019-03-04 22:43:20 +0700

    init v 1.0.1

--- src/components/TagsEdit.js
+++ src/components/TagsEdit.js
@@ -15,7 +15,7 @@ import Immutable from "immutable";
 
 import formGroup from "../js/formGroup";
 
-import "react-select/dist/react-select.css";
+// import "react-select/dist/react-select.css";
 import "../css/tagsedit.css";
 
 /**
